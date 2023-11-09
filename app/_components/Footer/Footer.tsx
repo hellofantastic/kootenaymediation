69769bf0b578377e6d1530bf75fbcc9ed59d89ae
Dallas <dlarsen@looominance.com>
2023-11-09 15:16:40 -0800

    Footer update grid row spacing and added address

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -9,10 +9,11 @@ export const Footer = () => {
   return (
     <BackgroundImageContainer>
       <Box className="footer" sx={{ a: { color: "#497173" } }} maxW={1200} m="auto" p="4rem 2rem 1rem 2rem">
-        <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }} gap={6}>
+        <Grid templateColumns={{ base: "1fr", md: "1fr 1fr 1fr" }} gap={6} rowGap={"1"}>
           <GridItem display="flex">
             <Link href="/">©{year} Kootenay Mediation</Link>
           </GridItem>
+          <GridItem>211-402 Baker St., Nelson B.C.</GridItem>
           <GridItem display="flex" justifyContent="right">
             <Link href="/privacy-policy">Privacy Policy</Link>
           </GridItem>
